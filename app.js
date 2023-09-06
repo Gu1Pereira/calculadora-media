2a669f0cb7f8fcb460fe73d387aa234c799ebd8f
Gu1Pereira <guilhermemap85@gmail.com>
2023-09-05 21:56:35 -0300

    cálculo de média

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+alert("pregiçoso")
+
+console.log("fica esperto") 
+
+console.log(50/5)
+
+alert(50/5)
+
+
+
+*/
+
+nota01 = 9
+nota02 = 8
+nota03 = 5
+nota04 = 8
+
+// " = " operador de atribuição: coloca oq esta na direita para a esquerda
+
+//tomar cuidado com letra maiuscula, acentos e etc  
+
+//console.log((nota01 + nota02 + nota03 + nota04)/4)
+
+media = (nota01 + nota02 + nota03 + nota04) / 4
+console.log(media)
+
+
+
